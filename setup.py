1b31e0fc4d14d6e04e208c8e43d67854854f3419
root <root@nas-server.localdomain>
2025-10-28 15:33:51 +0800

    until_done

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
+from tools import __version__, __author__, __author_email__
 
 setup(
     name='tools',
-    version='0.1.0',
+    version=__version__,
+    description='Python tools collection for various tasks.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author=__author__,
+    author_email=__author_email__,
+    url='https://github.com/emryslou/tools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[
-        'click',
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'tools=tools.cli:cli',
--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,4 +1,5 @@
 """Tools package initialization."""
 
 __version__ = "0.0.1"
-__author__ = "emrys.liu@foxmail.com"
+__author__ = "Emrys.Liu"
+__author_email__ = "emrys.liu@foxmail.com"
